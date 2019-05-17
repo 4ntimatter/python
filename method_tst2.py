20b59d0e5ae5a1c25db7d9f9b199713fa6447503
test1 <aryomhj@gmail.com>
2019-05-17 11:57:52 +0330

    method_tst

---
+++ method_tst2.py
@@ -0,0 +1,14 @@
+weight=input("yo dude tell me ur weight ya nqqa")
+hight= input("tell ya high too bro")
+weight=int (weight)
+hight=float(hight)
+blody_bmi= weight / (hight * hight)
+print(blody_bmi)
+if blody_bmi<20:
+    print("skinny")
+
+elif 20<blody_bmi<25:
+    print("busty")
+
+else: blody_bmi>25:
+    print("over")
---
+++ set.py
@@ -0,0 +1,13 @@
+text = """When the fever's set, the crowd is hot,
+The gloves are off, the knives are out
+And you're on your own
+And you're on your own
+Where the song has gone to grain
+Here the faceless conjugate and breed
+When the stage is set, the line is drawn
+The curtains up, the lights are on And you're on your own
+And you're on your own"""
+pr_re = text.split()
+print(pr_re)
+tex_set = set(pr_re)
+print(tex_set)
