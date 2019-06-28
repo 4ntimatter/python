4f69af48adad13d021d3af7e0bb543d9f261bae3
Student <aryomhj@gmail.com>
2019-06-28 12:06:27 +0330

    SO^

---
+++ senarios/colortst.py
@@ -0,0 +1,35 @@
+import sys
+
+class ColorPrint:
+
+    @staticmethod
+    def print_fail(message, end = '\n'):
+        sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_pass(message, end = '\n'):
+        sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_warn(message, end = '\n'):
+        sys.stderr.write('\x1b[1;33m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_info(message, end = '\n'):
+        sys.stdout.write('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_bold(message, end = '\n'):
+        sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
+
+def red(f):
+    def inner(*jargs, **kwarges):
+        return "\033[32:1m" + f(*jargs, **kwarges)+"\033(0m"
+
+
+
+
+
+
+ def echo(a):
+return a
